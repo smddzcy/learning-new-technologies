bdd4c2d8bed3601421f86e6789d0bdbc607c7b41
Samed Duzcay <samedduzcay@gmail.com>
2016-12-03 04:24:20 +0300

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Learning Haskell
+
+My learning progress of just another language, Haskell.
+
+## Checklist
+
+- [x] Introduction
+- [x] Types & typeclasses
+- [x] Function syntax
+- [x] Recursion
+- [ ] Higher Order Functions
+- [ ] Modules
+- [ ] Creating types & typeclasses
+- [ ] IO
+- [ ] Problem solving
+- [ ] Functors, Applicative Functors and Monoids
+- [ ] Monads
+- [ ] Zippers
+
+## Sources
+
+- http://learnyouahaskell.com/ - Very well written, fun to read.
+- https://www.haskell.org/documentation - Source of sources.
---
+++ helloWorld.hs
@@ -0,0 +1,39 @@
+-- Just sticking to the convention.
+firstString = "Hello, World!"
+main = print firstString
+
+-- A basic function declaration.
+-- Left of "=>" declares the typeclasses.
+-- Right part declares the input and output.
+doubleIt :: (Num a) => a -> a
+doubleIt a = a * 2
+
+-- A much basic function declaration.
+-- Output of ":t concatenate" = "concatenate :: [a] -> [a] -> [a]"
+concatenate a b = a ++ b
+
+-- Sorting
+
+-- Quicksort
+-- Pivot is the first element.
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smaller = quicksort [a | a <- xs, a <= x]
+        bigger  = quicksort [a | a <- xs, a > x]
+    in  smaller ++ [x] ++ bigger
+
+-- Mergesort
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+    | x <= y    = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
+
+mergesort :: (Ord a) => [a] -> [a]
+mergesort [] = []
+mergesort [x] = [x]
+mergesort x =
+    let (left, right) = splitAt (length x `div` 2) x
+    in merge (mergesort left) (mergesort right)
