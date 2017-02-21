e47ee39253dbcb3fdf277dc94d01294b4dd0c6b0
Samed Duzcay <samedduzcay@gmail.com>
2017-02-21 22:59:27 +0300

    Some more tensorflow

--- 12-tensorflow/playground.py
+++ 12-tensorflow/playground.py
@@ -34,3 +34,27 @@ init = tf.initialize_all_variables()
 sess.run(init)
 
 print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+
+# find the sum of squares error
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+# error is 31.5 here.
+
+# fix a and b with optimal parameters, which makes error 0
+fixa = tf.assign(a, [-1.])
+fixb = tf.assign(b, [1.])
+sess.run([fixa, fixb])
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+# fixing manually is not desirable of cours.
+# so we should train the model automatically
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+sess.run(init)  # reset values to incorrect defaults.
+for i in range(1000):
+    sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+
+print(sess.run([a, b]))
