2d6e58ec9e59141394f5ab3734008617d38bba85
Samed Duzcay <samedduzcay@gmail.com>
2017-03-09 13:31:48 +0300

    Better documentation and implement updates for prolog project (prolog)

--- 10-prolog/course-project/2015400027.pl
+++ 10-prolog/course-project/2015400027.pl
@@ -12,19 +12,22 @@ all_students(StudentList) :-
 
 % all_courses/1 is true iff CourseList contains all the courses.
 all_courses(CourseList) :-
+  % Get all the courses that students take in a flattened list and call it L2.
   findall(X, student(_, X), L1), flatten(L1, L2),
+  % Get rid of the duplicate courses in L2.
   list_to_set(L2, CourseList).
 
 % all_rooms/1 is true iff RoomList contains all the rooms.
 all_rooms(RoomList) :-
   findall(X, room_capacity(X, _), RoomList).
 
-% common_students/3 is true iff StudentCount is the common student count of
-% CourseID1 and CourseID2.
+% common_students/3 is true iff StudentCount is the common student count of CourseID1 and CourseID2.
 common_students(CourseID1, CourseID2, StudentCount) :-
-  students(CourseID1, Students1),
-  students(CourseID2, Students2),
+  % Get the students who takes CourseID1 and CourseID2.
+  students(CourseID1, Students1), students(CourseID2, Students2),
+  % Get the common students in lists Students1 and Students2.
   intersection(Students1, Students2, CommonStudents),
+  % Get the length of the common students.
   length(CommonStudents, StudentCount).
 
 % comb/3 generates N-element combinations of a list.
@@ -35,32 +38,45 @@ comb(N, [X|T], [X|Comb]) :-
 comb(N, [_|T], Comb) :-
   N > 0, comb(N, T, Comb).
 
-% slot_error/3 is true iff Error is the slot error of given 2 course final plans.
-%
-% Calculation of slot error:
-% If 2 course final plans have the same slot, slot error is the common student
-% count of those courses. Otherwise, it is 0.
-slot_error([CourseID1, _, Slot1], [CourseID2, _, Slot2], Error) :-
-  (  Slot1 == Slot2
-  -> (common_students(CourseID1, CourseID2, CommonStudentCount), Error is CommonStudentCount)
-  ;  Error is 0
-  ), !.
+% slot_error/3 is true iff Error is the slot error of given final plan parts.
+% If final plan parts have the same slot, slot error is the common student count of courses.
+% Otherwise, slot error is 0.
+slot_error([_, _, Slot1], [_, _, Slot2], 0) :-
+  Slot1 \= Slot2, !.
+slot_error([CourseID1, _, Slot1], [CourseID2, _, Slot1], Error) :-
+  common_students(CourseID1, CourseID2, Error).
 
 % slot_err_acc/3 is an accumulator for a fold in errors_for_plan/2.
 % It is true iff Error is the sum of PreviousError and slot error of
-% CourseFinalPlan1 and CourseFinalPlan2 (which is calculated by slot_error/3).
-slot_err_acc([CourseFinalPlan1, CourseFinalPlan2], PreviousError, Error) :-
-  slot_error(CourseFinalPlan1, CourseFinalPlan2, SlotError),
+% FinalPlanPart1 and FinalPlanPart2 (which is calculated by slot_error/3).
+slot_err_acc([FinalPlanPart1, FinalPlanPart2], PreviousError, Error) :-
+  slot_error(FinalPlanPart1, FinalPlanPart2, SlotError),
   Error is PreviousError + SlotError, !.
+
+% room_slot_conflict/3 is true iff Error is the room & slot conflict of given final plan parts.
+% Conflict is 0 if final plan parts have different room or slot.
+% Conflict is the sum of student counts if final plan parts have the same room and slot.
+room_slot_conflict([_, RoomID1, Slot1], [_, RoomID2, Slot2], 0) :-
+  (RoomID1 \= RoomID2; Slot1 \= Slot2), !.
+room_slot_conflict([CourseID1, RoomID1, Slot1], [CourseID2, RoomID1, Slot1], Error) :-
+  student_count(CourseID1, StudentCount1), student_count(CourseID2, StudentCount2),
+  Error is StudentCount1 + StudentCount2.
+
+% room_slot_conflict_acc/3 is an accumulator for a fold in conflicts_for_plan/2.
+% It is true iff Error is the sum of PreviousError and room & slot conflict of
+% FinalPlanPart1 and FinalPlanPart2 (which is calculated by room_slot_conflict/3).
+room_slot_conflict_acc([FinalPlanPart1, FinalPlanPart2], PreviousError, Error) :-
+  room_slot_conflict(FinalPlanPart1, FinalPlanPart2, RoomSlotConflict),
+  Error is PreviousError + RoomSlotConflict, !.
 
 % capacity_error_acc/3 is an accumulator for a fold in errors_for_plan/2.
 % It is true iff Error is the sum of PreviousError and capacity overflow of RoomID
 % assuming we assign the final exam of CourseID to RoomID.
 capacity_error_acc([CourseID, RoomID, _], PreviousError, Error) :-
   room_capacity(RoomID, RoomCapacity),
-  student_count(CourseID, NumberOfAttendee),
-  (  NumberOfAttendee > RoomCapacity
-  -> Error is NumberOfAttendee - RoomCapacity + PreviousError
+  student_count(CourseID, NumberOfAttendees),
+  (  NumberOfAttendees > RoomCapacity
+  -> Error is NumberOfAttendees - RoomCapacity + PreviousError
   ;  Error is PreviousError
   ), !.
 
@@ -74,15 +90,30 @@ errors_for_plan(Plan, ErrorCount) :-
   foldl(capacity_error_acc, Plan, SlotErrorCount, ErrorCount),
   !.
 
+% errors_for_plan/2 is true iff ErrorCount is the total error count of Plan.
+% Folds over the 2 element combinations of Plan, uses slot_err_acc/3,
+% capacity_error_acc/3 and slot_room_conflict/3 accumulators to get the count of
+% all errors of Plan.
+conflicts_for_plan([], 0) :- !.
+conflicts_for_plan(Plan, ErrorCount) :-
+  findall(X, comb(2, Plan, X), TwoCombinations),
+  foldl(slot_err_acc, TwoCombinations, 0, SlotErrorCount),
+  foldl(room_slot_conflict_acc, TwoCombinations, SlotErrorCount, RoomSlotErrorCount),
+  foldl(capacity_error_acc, Plan, RoomSlotErrorCount, ErrorCount),
+  !.
+
 % final_plan_generate_acc/3 is the accumulator for the fold in final_plan/1.
 % It is true if Plan is Course added to PreviousPlan with a proper room and slot
 % so that it has 0 error. It uses errors_for_plan/2 to calculate the error count.
 final_plan_generate_acc(Course, PreviousPlan, Plan) :-
-  available_slots(Slots), all_rooms(Rooms), !,
-  member(Slot, Slots), member(Room, Rooms),
-  PlanPart = [Course, Room, Slot],
-  append([PlanPart], PreviousPlan, Plan),
-  errors_for_plan(Plan, 0).
+  % Get all the slots and rooms, and pick one element from both.
+  available_slots(Slots), all_rooms(Rooms), !, member(Slot, Slots), member(Room, Rooms),
+  % Construct a part of the final plan and call it FinalPlanPart.
+  FinalPlanPart = [Course, Room, Slot],
+  % Append the final plan part to PreviousPlan.
+  append([FinalPlanPart], PreviousPlan, Plan),
+  % New plan, which is FinalPlanPart appended to PreviousPlan, should have 0 error.
+  conflicts_for_plan(Plan, 0).
 
 % final_plan/1 is true iff Plans is a course plan in the format
 % [[CourseID, RoomID, Slot], [CourseID2, RoomID2, Slot2], ...] with 0 errors.
@@ -94,9 +125,15 @@ final_plan(Plans) :-
 % clear_knowledge_base/0 retracts student/2, available_slots/1 and room_capacity/2,
 % and writes how many facts it has retracted.
 clear_knowledge_base :-
+  % Get the length of the student facts.
   all_students(Students), length(Students, StudentCount),
-  findall(X, available_slots(X), SlotDeclarations), length(SlotDeclarations, SlotDeclarationCount),
+  % Get the length of the slot facts.
+  findall(X, available_slots(X), Slots), length(Slots, SlotsCount),
+  % Get the length of the room facts.
   all_rooms(Rooms), length(Rooms, RoomCount),
+  % Retract the students and log the operation with the number of retracted student facts.
   retractall(student(_,_)), write('student/2: '), write(StudentCount), write('\n'),
-  retractall(available_slots(_)), write('available_slots/1: '), write(SlotDeclarationCount), write('\n'),
+  % Retract the slots and log the operation with the number of retracted slot facts.
+  retractall(available_slots(_)), write('available_slots/1: '), write(SlotsCount), write('\n'),
+  % Retract the rooms and log the operation with the number of retracted room facts.
   retractall(room_capacity(_,_)), write('room_capacity/2: '), write(RoomCount), write('\n').
--- 10-prolog/course-project/kb.pl
+++ 10-prolog/course-project/kb.pl
@@ -3,7 +3,8 @@ student(1, ['math101']).
 student(2, ['cmpe150']).
 
 :- dynamic available_slots/1.
-available_slots(['w-1', 'w-2']).
+available_slots(['w-1']).
 
 :- dynamic room_capacity/2.
 room_capacity('nh101', 5).
+room_capacity('nh102', 5).
