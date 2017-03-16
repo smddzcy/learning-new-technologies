5dca965349c476ad2ad2d29f2768339fb1c2a8f7
Samed Duzcay <samedduzcay@gmail.com>
2017-03-16 15:33:58 +0300

    Update exp 2 (verilog)

--- 11-verilog/exp2/source.v
+++ 11-verilog/exp2/source.v
@@ -4,12 +4,13 @@ module source(y, x);
 input wire [4:0] x;
 output wire [0:0] y;
 
-wire nx1, nx0, x1_or_x0, x1_and_x0, x1_and_nx0, nx1_and_x0, nx1_and_nx0, nDecOut2, x5;
+wire nx1, nx0, x1_or_x0, x1_and_x0, nx1_and_nx0;
 
 not(nx1, x[1]);
 not(nx0, x[0]);
 
-mydecoder dec(x[1], x[0], x1_and_x0, x1_and_nx0, nx1_and_x0, nx1_and_nx0);
+and(x1_and_x0, x[1], x[0]);
+and(nx1_and_nx0, nx1, nx0);
 
 not(x1_or_x0, nx1_and_nx0);
 
--- 11-verilog/exp2/testbench.v
+++ 11-verilog/exp2/testbench.v
@@ -7,7 +7,7 @@ wire y;
 source s(y, x);
 
 always begin
-    #20 x = x + 1;
+    #10 x = x + 1;
 end
 
 initial begin
@@ -15,7 +15,7 @@ initial begin
     $dumpvars(0, y, x);
 
     x = 5'b00000;
-    #640;
+    #320;
 
     $finish;
 end
